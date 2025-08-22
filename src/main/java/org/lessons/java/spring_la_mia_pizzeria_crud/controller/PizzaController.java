040423611f4ca3ede82a7d6c90c5ade37a881e22
MegaEmme <mikmusco@gmail.com>
2025-08-22 13:49:33 +0200

    feat: store, update, delete

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -7,10 +7,15 @@ import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -43,6 +48,50 @@ public class PizzaController {
         return "pizzas/show";
     }
 
+    // STORE (Chiamata GET per form da popolare e chiamata POST per inviare il form
+    // al db)
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "pizzas/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "pizzas/create";
+        }
+
+        repository.save(formPizza);
+        return "redirect:/pizzas";
+    }
+
+    // UPDATE (Chiamata GET per form da popolare e chiamata POST per inviare il form
+    // al db)
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("pizza", repository.findById(id).get());
+        return "pizzas/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "pizzas/edit";
+        }
+
+        repository.save(formPizza);
+        return "redirect:/pizzas";
+    }
+
+    // DELETE
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        repository.deleteById(id);
+        return "redirect:/pizzas";
+    }
     // QUERY CUSTOM TROVAPIZZADALNOME --> spostata direttamete nella index ^
     // @GetMapping("/searchByName")
     // public String searchByName(@RequestParam(name = "name") String name, Model
